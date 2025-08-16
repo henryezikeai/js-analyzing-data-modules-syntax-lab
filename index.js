e0c752e7428441af118a4f4ce7ea0f2e90f406cb
henryezikeai <henryezike@gmail.com>
2025-08-15 20:25:44 -0700

    done

--- index.js
+++ index.js
@@ -4,3 +4,18 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
+require('datejs'); 
+
+
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+  for (const arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
